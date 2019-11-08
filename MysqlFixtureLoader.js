6a30fe082f0cc024245645bedbd6b9b185a14b77
e33 <vitalii.svinchyak@gmail.com>
2019-11-08 14:02:17 +0200

    async/await

--- MysqlFixtureLoader.js
+++ MysqlFixtureLoader.js
@@ -1,91 +1,74 @@
-var Sequelize = require('sequelize');
-var MysqlFixtureLoader = function (Garden, config, logger) {
+const Sequelize = require('sequelize')
+const MysqlFixtureLoader = function (Garden, config, logger) {
 
-    var wait = Garden.wait;
-    var fs = require('fs');
-    var dbUri = config.get('fixtures-mysql:uri');
-    var sequelize_fixtures = require('sequelize-fixtures');
-    //var sequelize = new Sequelize(dbUri);
-    var sequelize = new Sequelize(dbUri, {logging: false});
-    var paths = config.get('fixtures-mysql:fixtures');
-    var modelsPath = config.get('fixtures-mysql:models');
-    var models = {};
+    const fs = require('fs')
+    const dbUri = config.get('fixtures-mysql:uri')
+    const sequelize_fixtures = require('sequelize-fixtures')
+    const sequelize = new Sequelize(dbUri, {logging: false})
+    const paths = config.get('fixtures-mysql:fixtures')
+    let modelsPath = config.get('fixtures-mysql:models')
+    const models = {}
 
-    function isAbsolute(path) {
-        return /^\//.test(path);
+    function isAbsolute (path) {
+        return /^\//.test(path)
     }
 
-    function loadModels() {
+    function loadModels () {
         if (!isAbsolute(modelsPath)) {
-            modelsPath = config.get('root_dir') + '/' + modelsPath;
+            modelsPath = config.get('root_dir') + '/' + modelsPath
         }
 
         fs.readdirSync(modelsPath).forEach(function (file) {
-            var nameParts = file.split("/");
-            var name = nameParts[(nameParts.length - 1)].split(".")[0];
-            models[name] = sequelize.import(modelsPath + '/' + file);
-        });
+            const nameParts = file.split('/')
+            const name = nameParts[(nameParts.length - 1)].split('.')[0]
+            models[name] = sequelize.import(modelsPath + '/' + file)
+        })
     }
 
-    function loadPath(path) {
-
+    async function loadPath (path) {
         if (!isAbsolute(path)) {
-            path = config.get('root_dir') + '/' + path;
+            path = config.get('root_dir') + '/' + path
         }
 
-        logger.info('fixtures: ' + path);
+        logger.info('fixtures: ' + path)
 
-        loadModels();
+        loadModels()
 
-        wait.for(function (callback) {
-            sequelize.sync({force: true}).on('success', function() {
-                logger.info('Mysql models synchronisation successfully!');
-                callback();
-            }).on('failure', function(err) {
-                console.log(err);
-                callback();
-            });
-        });
+        await sequelize.sync({force: true})
+        logger.info('Mysql models synchronisation successfully!')
 
-        wait.forMethod(sequelize_fixtures, 'loadFile', path + '/*.json', models, {log : function(){}});
+        await sequelize_fixtures.loadFile(path + '/*.json', models, {log: function () {}})
 
-        logger.info('success');
+        logger.info('success')
     }
 
-    this.load = function () {
+    this.load = async function () {
 
-        logger.info('Loading fixtures: ' + dbUri);
+        logger.info('Loading fixtures: ' + dbUri)
 
         if (paths instanceof Object) {
-            for (var key in paths) {
-                var path = paths[key];
-                loadPath(path);
+            for (const key in paths) {
+                const path = paths[key]
+                await loadPath(path)
             }
         } else {
-            loadPath(paths);
+            await loadPath(paths)
         }
-    };
+    }
 
-    this.drop = function () {
-        logger.info('Dropping fixtures: ' + dbUri);
+    this.drop = async function () {
+        logger.info('Dropping fixtures: ' + dbUri)
 
-        loadModels();
+        loadModels()
 
-        wait.for(function (callback) {
-            sequelize.drop({force: true}).on('success', function() {
-                callback();
-            }).on('failure', function(err) {
-                console.log(err);
-                callback();
-            });
-        });
+        await sequelize.drop({force: true})
 
-        logger.info('success');
-    };
+        logger.info('success')
+    }
 
-};
+}
 
-module.exports = MysqlFixtureLoader;
+module.exports = MysqlFixtureLoader
 
-module.exports.$inject = ['Garden', 'config', 'Logger'];
-module.exports.$tags = ['garden.js', 'fixtures', 'loader'];
+module.exports.$inject = ['Garden', 'config', 'Logger']
+module.exports.$tags = ['garden.js', 'fixtures', 'loader']
